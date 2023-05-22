a7f055ae6c1e412d7d1a0cc7c3313ef4ab1145d9
Eduardo Coyto <eduardocoyto@gmail.com>
2023-05-22 17:13:07 -0300

    se continua trabajando con los componentes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const ApiLastUser = "http://localhost:3002/api/user/last";
+const ApiLastProduct = "http://localhost:3002/api/product/last";
+
 function Home() {
+  let [lastUser, setLastUser] = useState([]);
+  let [lastProduct, setLastProduct] = useState([]);
+
+  useEffect(() => {
+    ansUser(ApiLastUser);
+    ansProduct(ApiLastProduct);
+  }, []);
+
+  const ansUser = async (ApiLastUser) => {
+    const res = await fetch(ApiLastUser);
+    const dataUser = await res.json();
+    console.log(dataUser);
+    setLastUser(dataUser.data);
+  };
+  const ansProduct = async (ApiLastProduct) => {
+    const res = await fetch(ApiLastProduct);
+    const dataProduct = await res.json();
+    setLastProduct(dataProduct.data);
+  
+  };
+
   return (
     <>
-      <section className="p-8 sm:p-12">
-        <div className="mb-8">
-          <h1 className="text-[40px] font-black text-center">Última vista</h1>
-        </div>
-      </section>
+      <div>
+        <h3 className="">{lastProduct[0]?.name}</h3>
+        <p className="">{lastProduct[0]?.price}</p>
+        <p className="">{lastProduct[0]?.description}</p>
+
+        <img
+          className=""
+          src={`http://localhost:3002/images/products/${lastProduct[0]?.Imagen[0]?.imagen}`}
+          alt="Imagen del producto"
+        /> 
+      </div>
+      <div>
+      <h3 className="">{`${lastUser[0]?.first_name} ${lastUser[0]?.last_name}`}</h3>
+        <p className="">{lastUser[0]?.email}</p>
+        
+      </div>
     </>
   );
 }
